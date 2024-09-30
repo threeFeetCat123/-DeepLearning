b70d4ef265c50baec0b2c948ae84019626b9ccd7
yinga he <747707368@qq.com>
2024-09-30 20:11:46 +0800

    ’fix:修改MP代码，从异或验证修改为加法器‘

--- MP/MP.py
+++ MP/MP.py
@@ -1,15 +1,11 @@
-
-
-
-
 from functools import reduce
 
 class Perceptron(object):
     def __init__(self, input_num, activator):
-        '''
+        """
         初始化感知器，设置输入参数的个数，以及激活函数f ->activator。
         激活函数的类型为double -> double
-        '''
+        """
         self.activator = activator
         # 权重向量初始化为0
         self.weights = [0.0 for _ in range(input_num)]
@@ -17,15 +13,15 @@ class Perceptron(object):
         self.bias = 0.0
 
     def __str__(self):
-        '''
+        """
         打印学习到的权重、偏置项
-        '''
+        """
         return 'weights\t:%s\nbias\t:%f\n' % (self.weights, self.bias)
 
     def predict(self, input_vec):
-        '''
+        """
         输入向量，输出感知器的计算结果
-        '''
+        """
         # 把input_vec[x1,x2,x3...]和weights[w1,w2,w3,...]打包在一起
         # 变成[(x1,w1),(x2,w2),(x3,w3),...]
         # 然后利用map函数计算[x1*w1, x2*w2, x3*w3]
@@ -39,16 +35,16 @@ class Perceptron(object):
                 , 0.0) + self.bias)
 
     def train(self, input_vecs, labels, iteration, rate):
-        '''
+        """
         输入训练数据：一组向量、与每个向量对应的label；以及训练轮数、学习率
-        '''
+        """
         for i in range(iteration):
             self._one_iteration(input_vecs, labels, rate)
 
     def _one_iteration(self, input_vecs, labels, rate):
-        '''
+        """
         一次迭代，把所有的训练数据过一遍
-        '''
+        """
         # 把输入和输出打包在一起，成为样本的列表[(input_vec, label), ...]
         # 而每个训练样本是(input_vec, label)
         samples = zip(input_vecs, labels)
@@ -60,9 +56,9 @@ class Perceptron(object):
             self._update_weights(input_vec, output, label, rate)
 
     def _update_weights(self, input_vec, output, label, rate):
-        '''
+        """
         按照感知器规则更新权重
-        '''
+        """
         # 把input_vec[x1,x2,x3,...]和weights[w1,w2,w3,...]打包在一起
         # 变成[(x1,w1),(x2,w2),(x3,w3),...]
         # 然后利用感知器规则更新权重
@@ -75,35 +71,35 @@ class Perceptron(object):
 
 
 def f(x):
-    '''
+    """
     定义激活函数f
-    '''
+    """
 #   这个不知道是啥玩意，自己捣鼓的，能实现MP不能整的加减法，但都是线性的，异或不行（异或要BP）
     return x
 #   下面的是经典MP
 #   return 1 if x > 0 else 0
 
 def get_training_dataset():
-    '''
+    """
     基于and真值表构建训练数据
-    '''
+    """
     # 构建训练数据
     # 输入向量列表
     input_vecs = [[1, 1], [0, 0], [1, 0], [0, 1]]
     # 期望的输出列表，注意要与输入一一对应
     # [1,1] -> 1, [0,0] -> 0, [1,0] -> 0, [0,1] -> 0
-    labels = [0, 0, 1, 1]  # 修改标签，使其符合 AND 的真值表
+    labels = [2, 0, 1, 1]  # 修改标签，使其符合 AND 的真值表
     return input_vecs, labels
 
 def train_and_perceptron():
-    '''
+    """
     使用and真值表训练感知器
-    '''
+    """
     # 创建感知器，输入参数个数为2（因为and是二元函数），激活函数为f
     p = Perceptron(2, f)
     # 训练，迭代10轮, 学习速率为0.1
     input_vecs, labels = get_training_dataset()
-    p.train(input_vecs, labels, 1000, 0.1)
+    p.train(input_vecs, labels, 1000, 0.01)
     #返回训练好的感知器
     return p
 
